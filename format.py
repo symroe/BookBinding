c4c92bdb2c0e5c90ea84baf19cb0aa61810a8938
Sym Roe <sym.roe@talusdesign.co.uk>
2011-03-25 20:00:54 +0000

    add support for folder of images

--- format.py
+++ format.py
@@ -7,12 +7,33 @@ import glob
 class BookMaker():
     def __init__(self, file_path):
         self.file_path = file_path
-    
-    def convert_pages(self):
-        file_name = os.path.split(self.file_path)[1]
+        self.file_name = os.path.split(self.file_path)[1]
         self.pdf_name = os.path.split(self.file_path)[1]
         self.book_name = self.pdf_name[:-4]
         self.out_file_name = "pages/%s" % self.book_name
+        
+    def convert_images_to_pages(self):
+        """
+        Sometimes we want to convert existing images (that have maybe already 
+        been converted from a pdf in some other way) in to pages (IE, cut in 
+        half and cropped.).
+        """
+        
+        args = [
+            'convert', 
+            '-crop',
+            '50%x100%',
+            '-trim',
+            '+repage',
+            '%s' % self.file_path,
+            "%s.png" % self.out_file_name]
+        
+        subprocess.call(" ".join(args), shell=True)
+        self.populate_pages()
+        
+        
+    
+    def convert_pages_from_pdf(self):
         args = [
             'convert', 
             '-density',
@@ -25,10 +46,15 @@ class BookMaker():
             "%s.png" % self.out_file_name]
         
         subprocess.call(" ".join(args), shell=True)
+        self.populate_pages()
+    
+    def populate_pages(self):
         self.pages = sorted(glob.glob("%s*" % self.out_file_name), key=self.parse_page_number)
         self.pages.insert(0, 'pages/blank.png')
+        
         self.num_pages = len(self.pages)
         self.group_pages()
+        
     
     def group_pages(self, n=8):
         """
@@ -85,11 +111,17 @@ class BookMaker():
 
 
 if __name__ == "__main__":
-    for pdf in glob.glob('pdfs/*.pdf'):
-        book = BookMaker(pdf)
-        book.convert_pages()
-        book.format_2_fold()
-
+    if 'image' in sys.argv:
+        for pdf in glob.glob('initial_images/*.png'):
+            book = BookMaker(pdf)
+            book.convert_images_to_pages()
+            book.format_2_fold()
+        
+    else:
+        for pdf in glob.glob('pdfs/*.pdf'):
+            book = BookMaker(pdf)
+            book.convert_pages_from_pdf()
+            book.format_2_fold()
 
 
 
